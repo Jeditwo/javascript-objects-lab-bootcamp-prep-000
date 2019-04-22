f8b667345bb33258ab5447ddf665f3147a479396
Joshua Wessels <joshuawessels04@gmail.com>
2019-04-22 15:57:22 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,18 @@
+var recipes = {
+  
+};
+
+function updateObjectWithKeyAndValue(obj, key, value) {
+  return Object.assign({}, obj, { [key]: value });
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
+  obj[key] = value;
+  return obj;
+}
+
+function deleteFromObjectByKey(object, key) {
+  var myvariable = Object.assign({}, object);
+  delete myvariable.[key];
+  return myvariable;
+}
